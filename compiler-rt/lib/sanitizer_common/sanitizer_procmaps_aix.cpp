fcc59f1ada360e9c574b172ba333517251fc0da1
Jake Egan <jake.egan@ibm.com>
2026-03-17 01:25:16 -0400

    Address comments

--- compiler-rt/lib/sanitizer_common/sanitizer_procmaps_aix.cpp
+++ compiler-rt/lib/sanitizer_common/sanitizer_procmaps_aix.cpp
@@ -12,7 +12,6 @@
 #include "sanitizer_platform.h"
 
 #if SANITIZER_AIX
-#  include <assert.h>
 #  include <stdio.h>
 #  include <stdlib.h>
 #  include <sys/procfs.h>
@@ -21,11 +20,11 @@
 #  include "sanitizer_file.h"
 #  include "sanitizer_procmaps.h"
 
-namespace __sanitizer {
+using namespace __sanitizer;
 
 static int qsort_comp(const void *va, const void *vb) {
-  const prmap_t *a = (const prmap_t *)va;
-  const prmap_t *b = (const prmap_t *)vb;
+  auto *a = static_cast<const prmap_t *>(va);
+  auto *b = static_cast<const prmap_t *>(vb);
 
   if (a->pr_vaddr < b->pr_vaddr)
     return -1;
@@ -33,15 +32,17 @@ static int qsort_comp(const void *va, const void *vb) {
   if (a->pr_vaddr > b->pr_vaddr)
     return 1;
 
+  CHECK_EQ(a, b);
   return 0;
 }
 
-static prmap_t *SortProcMapEntries(char *buffer) {
-  prmap_t *begin = (prmap_t *)buffer;
+static prmap_t *SortProcMapEntries(char *buffer, uptr len) {
+  prmap_t *begin = reinterpret_cast<prmap_t *>(buffer);
+  prmap_t *bufferEnd = reinterpret_cast<prmap_t *>(buffer + len);
   prmap_t *mapIter = begin;
   // The AIX procmap utility detects the end of the array of `prmap`s by finding
   // an entry where pr_size and pr_vaddr are both zero.
-  while (mapIter->pr_size != 0 || mapIter->pr_vaddr != 0) ++mapIter;
+  while (mapIter < bufferEnd && (mapIter->pr_size != 0 || mapIter->pr_vaddr != 0)) ++mapIter;
   prmap_t *end = mapIter;
 
   size_t count = end - begin;
@@ -51,11 +52,13 @@ static prmap_t *SortProcMapEntries(char *buffer) {
   return end;
 }
 
-void ReadProcMaps(ProcSelfMapsBuff *proc_maps) {
+void __sanitizer::ReadProcMaps(ProcSelfMapsBuff *proc_maps) {
   uptr pid = internal_getpid();
   constexpr unsigned BUFFER_SIZE = 128;
   char filenameBuf[BUFFER_SIZE] = {};
-  internal_snprintf(filenameBuf, BUFFER_SIZE, "/proc/%d/map", pid);
+  int filenameLen = internal_snprintf(filenameBuf, BUFFER_SIZE, "/proc/%d/map", pid);
+  CHECK_GE(filenameLen, 0);
+  CHECK_LT(filenameLen, static_cast<int>(BUFFER_SIZE));
   if (!ReadFileToBuffer(filenameBuf, &proc_maps->data, &proc_maps->mmaped_size,
                         &proc_maps->len)) {
     proc_maps->data = nullptr;
@@ -65,14 +68,14 @@ void ReadProcMaps(ProcSelfMapsBuff *proc_maps) {
     return;
   }
 
-  proc_maps->mapEnd = SortProcMapEntries(proc_maps->data);
+  proc_maps->mapEnd = SortProcMapEntries(proc_maps->data, proc_maps->len);
 }
 
-bool MemoryMappingLayout::Next(MemoryMappedSegment *segment) {
+bool __sanitizer::MemoryMappingLayout::Next(MemoryMappedSegment *segment) {
   if (Error())
     return false;  // simulate empty maps
 
-  const prmap_t *mapIter = (const prmap_t *)data_.current;
+  auto *mapIter = reinterpret_cast<const prmap_t *>(data_.current);
 
   if (mapIter >= data_.proc_self_maps.mapEnd)
     return false;
@@ -84,6 +87,8 @@ bool MemoryMappingLayout::Next(MemoryMappedSegment *segment) {
   if (mapIter >= data_.proc_self_maps.mapEnd)
     return false;
 
+  // The following has to be a C-style cast because the source type requires
+  // static_cast in 32-bit mode and reinterpret_cast in 64-bit mode.
   segment->start = (uptr)mapIter->pr_vaddr;
   segment->end = segment->start + mapIter->pr_size;
 
@@ -100,33 +105,31 @@ bool MemoryMappingLayout::Next(MemoryMappedSegment *segment) {
   if (type == MA_SLIBTEXT || type == MA_PLIBDATA)
     segment->protection |= kProtectionShared;
 
+  // Handle filenames for loaded objects.
+  // pr_pathoff is non-zero if the map entry is for a loaded object.
   if (segment->filename && mapIter->pr_pathoff) {
-    uptr len;
-    constexpr unsigned BUFFER_SIZE = 128;
-    char objPath[BUFFER_SIZE] = {};
-    // Use path /proc/<pid>/object/<object_id>
+    // Use path /proc/<pid>/object/<object_id>.
     // TODO: Pass a separate path from mapIter->pr_pathoff to display to the
     // user.
-    // TODO: Append the archive member name if it exists.
-    internal_snprintf(objPath, BUFFER_SIZE, "/proc/%d/object/%s",
-                      internal_getpid(), mapIter->pr_mapname);
-    len = Min((uptr)internal_strlen(objPath), segment->filename_size - 1);
-    internal_strncpy(segment->filename, objPath, len);
-    segment->filename[len] = 0;
-
+    // FIXME: Append the archive member name if it exists.
+    int objPathLen = internal_snprintf(segment->filename, segment->filename_size,
+      "/proc/%d/object/%s", internal_getpid(), mapIter->pr_mapname);
+    if (objPathLen < 0)
+      segment->filename[0] = 0;
   } else if (segment->filename) {
     segment->filename[0] = 0;
   }
 
-  assert(mapIter->pr_off == 0 && "expect a zero offset into module.");
+  // AIX does not report the offset into any loaded modules. The offset into the module
+  // is best handled by recording the segment type and having the symbolizer determine
+  // the offset using that.
+  // FIXME: Record the segment type.
   segment->offset = 0;
 
   ++mapIter;
-  data_.current = (const char *)mapIter;
+  data_.current = reinterpret_cast<const char *>(mapIter);
 
   return true;
 }
-
-}  // namespace __sanitizer
 
 #endif  // SANITIZER_AIX
